3540bdb22887a7abfc9421f58fa24dad73a70072
samiul-ahm <samiulahm09@gmail.com>
2025-12-16 20:28:54 +0600

    added new users and request

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 4000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 app.use(cors());
@@ -11,45 +11,57 @@ app.use(express.json());
 // missionScic11
 // a11wy7frFYHhH0nN
 
-
-const uri = "mongodb+srv://missionScic11:a11wy7frFYHhH0nN@cluster0.pej65ns.mongodb.net/?appName=Cluster0";
+const uri =
+  "mongodb+srv://missionScic11:a11wy7frFYHhH0nN@cluster0.pej65ns.mongodb.net/?appName=Cluster0";
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     await client.connect();
 
-   const database = client.db('missionscic11DB'); 
-   const userCollection = database.collection('user');
+    const database = client.db("missionscic11DB");
+    const userCollection = database.collection("user");
+    const requestsCollection = database.collection("requests");
 
-   app.post('/users', async(req, res)=>{
-    const userInfo = req.body;
-    userInfo.role = "Buyer";
-    userInfo.createdAt = new Date();
+    //
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      userInfo.createdAt = new Date();
+      userInfo.role = "donar";
+      userInfo.status = "active";
+      const result = await userCollection.insertOne(userInfo);
 
-    const result = await userCollection.insertOne(userInfo);
+      res.send(result);
+    });
 
-    res.send(result);
-   })
+    // requests
 
+    app.post("/requests", async (req, res) => {
+      const data = req.body;
+      data.CreatedAt = new Date();
+      const result = await requestsCollection.insertOne(data);
+      res.send(result);
+    });
 
-   app.get('/users/role/:email', async (req, res)=>{
-    const email = req.params.email;
-    const query = {email:email}
-    const result = await userCollection.findOne(query);
-    console.log(result);
-    res.send(result);
-   })
+    app.get("/users/role/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -57,12 +69,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Hello E11");
+});
 
-
-app.get("/", (req, res)=>{
-    res.send("Hello E11");
-})
-
-app.listen(port, ()=>{
-    console.log(`app running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
